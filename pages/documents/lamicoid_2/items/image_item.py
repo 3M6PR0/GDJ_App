345dcf0d4efa9e109627e24dbec0bb23197a00ec
Manseau33 <etiennemgodin@gmail.com>
2025-06-13 15:44:55 -0400

    Ajout et correction de la toolbar Lamicoid : boutons texte, image, gras, italique, souligné dynamiques

--- pages/documents/lamicoid_2/items/image_item.py
+++ pages/documents/lamicoid_2/items/image_item.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QGraphicsItem
-from PyQt5.QtGui import QPixmap, QPainter
+from PyQt5.QtGui import QPixmap, QPainter, QImage
 from PyQt5.QtCore import QRectF, Qt
 from .base_item import EditableItemBase
 from models.documents.lamicoid_2.elements import ElementImage
@@ -15,8 +15,21 @@ class ImageItem(EditableItemBase):
     def paint(self, painter: QPainter, option, widget=None):
         super().paint(painter, option, widget)
         if not self.pixmap.isNull():
-            # Dessiner l'image à la taille de l'élément
-            painter.drawPixmap(self.rect().toRect(), self.pixmap)
+            target_rect = self.rect().toRect()
+            # Utiliser QImage pour un scaling de meilleure qualité
+            image = self.pixmap.toImage()
+            if target_rect.width() <= self.pixmap.width() and target_rect.height() <= self.pixmap.height():
+                # On réduit seulement (jamais agrandir pour éviter la pixellisation)
+                scaled_img = image.scaled(target_rect.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+                pixmap_scaled = QPixmap.fromImage(scaled_img)
+                x = target_rect.x() + (target_rect.width() - pixmap_scaled.width()) // 2
+                y = target_rect.y() + (target_rect.height() - pixmap_scaled.height()) // 2
+                painter.drawPixmap(x, y, pixmap_scaled)
+            else:
+                # Si la bounding box est plus grande, on centre l'image à sa taille réelle
+                x = target_rect.x() + (target_rect.width() - self.pixmap.width()) // 2
+                y = target_rect.y() + (target_rect.height() - self.pixmap.height()) // 2
+                painter.drawPixmap(x, y, self.pixmap)
         else:
             painter.setPen(Qt.red)
             painter.drawRect(self.rect())
